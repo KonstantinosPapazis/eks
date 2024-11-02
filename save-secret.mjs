5b3495cb20cb4d771142953d2230c878c483f338
Konstantinos Papazis <konstantinospapazis@kpapazis.home>
2024-11-02 17:45:18 +0100

    modify actions

--- save-secret.mjs
+++ save-secret.mjs
@@ -1,10 +1,7 @@
 import { Octokit } from '@octokit/rest';
-import * as sodium from 'libsodium-wrappers';
+import sodium from 'tweetsodium';
 
 (async () => {
-    // Wait for sodium to be ready
-    await sodium.ready;
-
     const octokit = new Octokit({ auth: process.env.TPA_CLASSIC_TOKEN });
     const owner = 'KonstantinosPapazis';
     const repo = 'deployments';
@@ -14,10 +11,10 @@ import * as sodium from 'libsodium-wrappers';
     // Fetch the public key for encryption
     const { data: { key, key_id } } = await octokit.actions.getRepoPublicKey({ owner, repo });
 
-    // Encrypt the secret value using crypto_box_seal
+    // Encrypt the secret value
     const messageBytes = Buffer.from(secretValue);
     const keyBytes = Buffer.from(key, 'base64');
-    const encryptedBytes = sodium.crypto_box_seal(messageBytes, keyBytes); // Correct encryption function for libsodium-wrappers
+    const encryptedBytes = sodium.seal(messageBytes, keyBytes); // Correct encryption method
     const encryptedValue = Buffer.from(encryptedBytes).toString('base64');
 
     // Store the encrypted secret
